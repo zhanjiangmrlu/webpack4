105a7489af99a2c4b840f07f32de4808c5b7c9c1
lufengkai <zhanjiangmrlu@163.com>
2019-05-04 22:38:05 +0800

    feat: 移除部分没用的代码

--- src/inxdex.js
+++ src/inxdex.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  // 将图片添加到我们现有的div
-  let myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log('Data', Data);
   
   return element;
 }
